5ab1b1590570966ff8b4734cabaadedd33d8a388
ernist <ernist.xu@gmail.com>
2013-09-18 05:34:54 +0800

    s

---
+++ Hello-EMEL/Hello-EMEL/tw_com_goldrock_SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  tw_com_goldrock_SecondViewController.h
+//  
+//
+//  Created by ernist on 13/9/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tw_com_goldrock_SecondViewController : UIViewController
+
+@end
---
+++ Hello-EMEL/Hello-EMEL/tw_com_goldrock_SecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  tw_com_goldrock_SecondViewController.m
+//  
+//
+//  Created by ernist on 13/9/18.
+//
+//
+
+#import "tw_com_goldrock_SecondViewController.h"
+
+@interface tw_com_goldrock_SecondViewController ()
+
+@end
+
+@implementation tw_com_goldrock_SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
